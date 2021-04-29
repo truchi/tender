eb96f4d4da8d323e63b665a271822f5659cac745
Romain TRUCHI <romain.truchi.06@gmail.com>
2021-04-29 14:48:25 +0200

    Private grounds and Color

--- src/style/color/mod.rs
+++ src/style/color/mod.rs
@@ -1,4 +1,4 @@
-//! Colors ([`Color`]: [`Rgb`], [`Rgba`], [`PreRgba`]).
+//! Colors ([`Rgb`], [`Rgba`], [`PreRgba`]).
 
 mod ground;
 mod pre_rgba;
@@ -6,7 +6,7 @@ mod rgb;
 mod rgba;
 mod with_alpha;
 
-pub use ground::*;
+pub(super) use ground::*;
 pub use pre_rgba::*;
 pub use rgb::*;
 pub use rgba::*;
@@ -16,13 +16,7 @@ use super::*;
 
 /// A wrapper type for colors.
 #[derive(Copy, Clone, Eq, Default, Hash, Debug)]
-pub struct Color<T>(pub T);
-
-impl<T> From<T> for Color<T> {
-    fn from(t: T) -> Self {
-        Self(t)
-    }
-}
+pub(super) struct Color<T>(pub T);
 
 impl<T: PartialEq<U>, U> PartialEq<Color<U>> for Color<T> {
     fn eq(&self, other: &Color<U>) -> bool {
