70f2e6937940d2c05c6fbb0fa3b51dd3700b4345
Romain TRUCHI <romain.truchi.06@gmail.com>
2021-04-30 01:08:38 +0200

    Rust ICE

--- src/canvas/layer.rs
+++ src/canvas/layer.rs
@@ -1,5 +1,6 @@
 use super::*;
 
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Default, Debug)]
 pub struct Layer<T> {
     pub position: Point,
     pub grid:     T,
@@ -11,20 +12,6 @@ impl<T> Layer<T> {
             position: position.into(),
             grid,
         }
-    }
-}
-
-impl<'t, 'b, Top, Bottom> Over<&'b mut Layer<Bottom>, ()> for &'t Layer<Top>
-where
-    &'t Top: GridRows,
-    &'b mut Bottom: GridRows,
-    <&'t Top as Grid>::Item: Over<<&'b mut Bottom as Grid>::Item, ()>,
-{
-    fn over(self, bottom: &'b mut Layer<Bottom>) {
-        self.grid
-            .zip_at(bottom.position(), &mut bottom.grid)
-            .flatten_rows()
-            .for_each(|(top, bottom)| top.over(bottom));
     }
 }
 
@@ -40,7 +27,7 @@ impl<T> WithPosition for Layer<T> {
     }
 }
 
-macro_rules! grid {
+macro_rules! _grid {
     ($($Trait:ident $Assoc:ident $fn:ident $Index:ident)*) => { $(
         grid!(impl $Trait $Assoc $fn $Index);
         grid!(impl $Trait $Assoc $fn $Index mut);
@@ -60,8 +47,48 @@ macro_rules! grid {
     };
 }
 
-grid!(
-    Grid     Item item_unchecked Index0D
-    GridRow  Row  row_unchecked  Index1D
-    GridRows Rows rows_unchecked Index2D
-);
+// FIXME this makes weird stackoverflow compilation error when doing:
+// `layer.over(layer)`
+// grid!(
+// Grid     Item item_unchecked Index0D
+// GridRow  Row  row_unchecked  Index1D
+// GridRows Rows rows_unchecked Index2D
+// );
+
+impl<'t, 'b, Top, Bottom> Over<&'b mut Layer<Bottom>, ()> for &'t Layer<Top>
+where
+    &'t Top: GridRows,
+    &'b mut Bottom: GridRows,
+    <&'t Top as Grid>::Item: Over<<&'b mut Bottom as Grid>::Item, ()>,
+{
+    fn over(self, bottom: &'b mut Layer<Bottom>) {
+        self.grid
+            .zip_at(bottom.position(), &mut bottom.grid)
+            .flatten_rows()
+            .for_each(|(top, bottom)| top.over(bottom));
+    }
+}
+
+fn main() {
+    let canvas_cell = Cell::<Rgb, _>::new(' ', Rgb(0, 0, 0), Rgb(255, 0, 0), Default::default());
+    let mut canvas = Layer::new(
+        (0, 0),
+        RowVec1D::new((151, 40), vec![canvas_cell; 151 * 40]).unwrap(),
+    );
+
+    let cell1 = Cell::<Rgb, _>::new('a', Rgb(0, 255, 0), Rgba(0, 0, 0, 127), Default::default());
+    let layer1 = Layer::new((1, 1), repeat((40, 20), cell1));
+
+    let cell2 = Cell::<Rgb, _>::new(
+        'b',
+        Rgb(0, 0, 255),
+        Rgba(0, 255, 0, 127),
+        Default::default(),
+    );
+    let layer2 = Layer::new((2, 2), repeat((40, 20), cell2));
+
+    dbg!(layer1);
+    dbg!(layer2);
+
+    (&layer1).over(&mut canvas);
+}
