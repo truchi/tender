9d413fcbd76d80221a8edfdd8b290204bfd6d105
Romain TRUCHI <romain.truchi.06@gmail.com>
2021-05-15 14:20:13 +0200

    Under trait

--- src/style/mod.rs
+++ src/style/mod.rs
@@ -16,15 +16,19 @@ pub trait Over<Bottom> {
     fn over(self, bottom: Bottom) -> Self::Output;
 }
 
-pub trait Paint<Top, Output> {
-    fn paint(self, top: Top) -> Output;
+pub trait Under<Top> {
+    type Output;
+
+    fn under(self, top: Top) -> Self::Output;
 }
 
-// impl<T, Top: Over<T, Output>, Output> Paint<Top, Output> for T {
-// fn paint(self, top: Top) -> Output {
-// top.over(self)
-// }
-// }
+impl<Top: Over<Bottom>, Bottom> Under<Top> for Bottom {
+    type Output = Top::Output;
+
+    fn under(self, top: Top) -> Self::Output {
+        top.over(self)
+    }
+}
 
 pub struct Dedup<T>(pub T, pub T);
 
