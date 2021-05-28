f36b74ad58574338817f7b4114e23649c7e0bd8a
Romain TRUCHI <romain.truchi.06@gmail.com>
2021-05-28 14:12:56 +0200

    Crop: Grid* impl for ref/ref mut

--- src/grid/crop.rs
+++ src/grid/crop.rs
@@ -31,58 +31,119 @@ impl<T> WithSize for Crop<T> {
     }
 }
 
-impl<T: Grid> Grid for Crop<T> {
-    type Item = T::Item;
+macro_rules! grid0d {
+    () => {
+        grid0d!(impl owned);
+        grid0d!(impl ref);
+        grid0d!(impl ref mut);
+    };
+    (impl owned) => {
+        impl<T> Grid for Crop<T>
+        where
+            T: Grid
+        {
+            type Item = T::Item;
 
-    unsafe fn item_unchecked(self, index: impl Index0D) -> Self::Item {
-        let mut index = index.unchecked();
-        index.x += self.rect.x.start;
-        index.y += self.rect.y.start;
+            grid0d!(impl fn);
+        }
+    };
+    (impl ref $($mut:ident)?) => {
+        impl<'a, T> Grid for &'a $($mut)? Crop<&$($mut)? T>
+        where
+            &'a $($mut)? T: Grid
+        {
+            type Item = <&'a $($mut)? T as Grid>::Item;
 
-        self.grid.item_unchecked(index)
-    }
+            grid0d!(impl fn);
+        }
+    };
+    (impl fn) => {
+        unsafe fn item_unchecked(self, index: impl Index0D) -> Self::Item {
+            let index = index.unchecked() + self.rect.start();
+
+            self.grid.item_unchecked(index)
+        }
+    };
 }
 
 macro_rules! grid1d {
     ($($Trait:ident $Assoc:ident $fn:ident $i:ident $range:ident)*) => { $(
-        impl<T: $Trait> $Trait for Crop<T> {
+        grid1d!(impl owned  : $Trait $Assoc $fn $i $range);
+        grid1d!(impl ref    : $Trait $Assoc $fn $i $range);
+        grid1d!(impl ref mut: $Trait $Assoc $fn $i $range);
+    )* };
+    (impl owned: $Trait:ident $Assoc:ident $fn:ident $i:ident $range:ident) => {
+        impl<T> $Trait for Crop<T>
+        where
+            T: $Trait
+        {
             type $Assoc = T::$Assoc;
 
-            unsafe fn $fn(self, index: impl Index1D) -> Self::$Assoc {
-                let mut index = index.$fn(self.size());
-                index.0 += self.rect.$i.start;
-                index.1.start += self.rect.$range.start;
-                index.1.end += self.rect.$range.start;
-
-                self.grid.$fn(index)
-            }
+            grid1d!(impl fn $fn $Assoc $i $range);
         }
-    )* };
+    };
+    (impl ref $($mut:ident)?: $Trait:ident $Assoc:ident $fn:ident $i:ident $range:ident) => {
+        impl<'a, T> $Trait for &'a $($mut)? Crop<&$($mut)? T>
+        where
+            &'a $($mut)? T: $Trait
+        {
+            type $Assoc = <&'a $($mut)? T as $Trait>::$Assoc;
+
+            grid1d!(impl fn $fn $Assoc $i $range);
+        }
+    };
+    (impl fn $fn:ident $Assoc:ident $i:ident $range:ident) => {
+        unsafe fn $fn(self, index: impl Index1D) -> Self::$Assoc {
+            let mut index = index.$fn(self.size());
+            index.0 += self.rect.$i.start;
+            index.1.start += self.rect.$range.start;
+            index.1.end += self.rect.$range.start;
+
+            self.grid.$fn(index)
+        }
+    };
 }
 
 macro_rules! grid2d {
     ($($Trait:ident $Assoc:ident $fn:ident)*) => { $(
-        impl<T: $Trait> $Trait for Crop<T> {
+        grid2d!(impl owned  : $Trait $Assoc $fn);
+        grid2d!(impl ref    : $Trait $Assoc $fn);
+        grid2d!(impl ref mut: $Trait $Assoc $fn);
+    )* };
+    (impl owned: $Trait:ident $Assoc:ident $fn:ident) => {
+        impl<T> $Trait for Crop<T>
+        where
+            T: $Trait
+        {
             type $Assoc = T::$Assoc;
 
-            unsafe fn $fn(self, index: impl Index2D) -> Self::$Assoc {
-                let mut index = index.unchecked(self.size());
-                index.x.start += self.rect.x.start;
-                index.y.start += self.rect.y.start;
-                index.x.end += self.rect.x.start;
-                index.y.end += self.rect.y.start;
-
-                self.grid.$fn(index)
-            }
+            grid2d!(impl fn $fn $Assoc);
         }
-    )* };
+    };
+    (impl ref $($mut:ident)?: $Trait:ident $Assoc:ident $fn:ident) => {
+        impl<'a, T> $Trait for &'a $($mut)? Crop<&$($mut)? T>
+        where
+            &'a $($mut)? T: $Trait
+        {
+            type $Assoc = <&'a $($mut)? T as $Trait>::$Assoc;
+
+            grid2d!(impl fn $fn $Assoc);
+        }
+    };
+    (impl fn $fn:ident $Assoc:ident) => {
+        unsafe fn $fn(self, index: impl Index2D) -> Self::$Assoc {
+            let index = index.unchecked(self.size()).translate(self.rect.start());
+
+            self.grid.$fn(index)
+        }
+    };
 }
 
+grid0d!();
 grid1d!(
     GridCol Col col_unchecked x y
     GridRow Row row_unchecked y x
 );
-
 grid2d!(
     GridCols  Cols  cols_unchecked
     GridRows  Rows  rows_unchecked
